7f811ad859bfd5980a6e98cb371e2dfb01181eee
Varun Thakore <vrnthakore@gmail.com>
2025-01-12 02:16:50 +0530

    Update Record

--- utils/src/main.rs
+++ utils/src/main.rs
@@ -4,10 +4,8 @@ use csv::{ReaderBuilder, WriterBuilder};
 use serde::{Deserialize, Serialize};
 use std::{
     error::Error,
-    fs::{self, File},
-    io::Write,
+    fs,
     process::Command,
-    time::{Duration, Instant},
 };
 
 /// A tool to build and optionally benchmark a cargo project
@@ -39,10 +37,14 @@ struct Cli {
 #[derive(Deserialize, Serialize, Debug)]
 struct Record {
     n: String,
-    #[serde(rename = "time(ms)")]
+    #[serde(rename = "prover time(ms)")]
     time_ms: u64,
     #[serde(rename = "proof size(bytes)")]
     proof_size: u64,
+    #[serde(rename = "verifier time(ms)")]
+    verifier_time_ms: u64,
+    #[serde(rename = "cycle count")]
+    cycle_count: u64,
     #[serde(rename = "peak memory")]
     memory: String,
 }
@@ -74,7 +76,7 @@ fn main() {
             // if it is not stone library
             Command::new("heaptrack")
                 .arg("-o")
-                .arg("heaptrack-prover")
+                .arg(format!("{}-{}", name, bench_arg))
                 .arg(cli.bin)
                 .arg("--n")
                 .arg(&bench_arg)
@@ -87,7 +89,8 @@ fn main() {
         // Analyze the heaptrack results
         let output = Command::new("heaptrack")
             .arg("-a")
-            .arg("heaptrack-prover.zst") // Ensure the filename matches what heaptrack produced
+            .arg(format!("{}-{}.zst", name, bench_arg))
+            // .arg("heaptrack-prover.zst") // Ensure the filename matches what heaptrack produced
             .output()
             .expect("Failed to analyze the heaptrack results");
 
@@ -110,6 +113,8 @@ fn main() {
         update_or_insert_record(
             &file,
             &bench_arg,
+            None,
+            None,
             None,
             None,
             Some(peak_value_str.to_string()),
@@ -141,7 +146,19 @@ fn main() {
             .as_u64()
             .expect("Failed to convert duration to u64");
 
-        update_or_insert_record(&file, &bench_arg, Some(duration), Some(proof_size), None)
+        let verifier_duration = json
+            .get("verifier_duration")
+            .expect("Failed to get duration")
+            .as_u64()
+            .expect("Failed to convert duration to u64");
+
+        let cycle_count = json
+            .get("cycle_count")
+            .expect("Failed to get duration")
+            .as_u64()
+            .expect("Failed to convert duration to u64");
+
+        update_or_insert_record(&file, &bench_arg, Some(duration), Some(proof_size), Some(verifier_duration), Some(cycle_count), None)
             .expect("Failed to update or insert record");
 
         // remove json file
@@ -154,6 +171,8 @@ fn update_or_insert_record(
     bench_arg: &str,
     duration: Option<u64>,
     proof_size: Option<u64>,
+    verifier_duration: Option<u64>,
+    cycle_count: Option<u64>,
     memory: Option<String>,
 ) -> Result<(), Box<dyn Error>> {
     let file_exists = fs::metadata(file_path).is_ok();
@@ -180,6 +199,12 @@ fn update_or_insert_record(
             if let Some(proof_size) = proof_size {
                 record.proof_size = proof_size;
             }
+            if let Some(verifier_duration) = verifier_duration {
+                record.verifier_time_ms = verifier_duration;
+            }
+            if let Some(cycle_count) = cycle_count {
+                record.cycle_count = cycle_count;
+            }
             if let Some(ref memory_str) = memory {
                 record.memory = memory_str.clone();
             }
@@ -192,11 +217,15 @@ fn update_or_insert_record(
     if !updated {
         let duration = duration.unwrap_or(0);
         let proof_size = proof_size.unwrap_or(0);
+        let verifier_duration = verifier_duration.unwrap_or(0);
+        let cycle_count = cycle_count.unwrap_or(0);
         let memory = memory.unwrap_or(0.to_string());
         records.push(Record {
             n: bench_arg.to_string(),
             time_ms: duration,
             proof_size,
+            verifier_time_ms: verifier_duration,
+            cycle_count,
             memory,
         });
     }

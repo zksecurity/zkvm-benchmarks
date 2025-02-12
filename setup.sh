39e9aa8b8ead4240595ed5f48e47240454a9e986
varunthakore <vrnthakore@gmail.com>
2025-02-12 12:07:21 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,5 +4,38 @@ echo "Setting up benchmark..."
 
 whoami
 
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/env:$PATH"
+
+# Install other dependencies
 sudo apt update -y
 sudo apt install -y heaptrack
+sudo apt install -y build-essential
+sudo apt install -y pkg-config
+sudo apt install -y libssl-dev
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo apt install -y libgmp-dev
+sudo apt install -y clang
+sudo apt install -y age
+sudo apt install -y just
+
+# Install Jolt
+rustup target add riscv32i-unknown-none-elf
+
+# Install Risc Zero
+curl -L https://risczero.com/install | bash
+export PATH="$HOME/.risc0/bin:$PATH"
+rzup install
+
+# Install SP1
+curl -L https://sp1.succinct.xyz | bash
+export PATH="$HOME/.sp1/bin:$PATH"
+sp1up
+
+# Install stone-cli
+cargo install --git https://github.com/zksecurity/stone-cli.git --branch dynamic-layout
+
+# Install cairo
+pip install cairo-lang

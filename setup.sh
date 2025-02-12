56a337e1dbba94d34c51f41c89fca306da551a2f
varunthakore <vrnthakore@gmail.com>
2025-02-12 06:57:26 +0000

    fix setup errors

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Setting up benchmark..."
 whoami
 
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Install other dependencies
 sudo apt update -y
@@ -18,16 +18,19 @@ sudo apt install -y python3-venv
 sudo apt install -y libgmp-dev
 sudo apt install -y clang
 sudo apt install -y age
+sudo apt install -y just
 
 # Install Jolt
 rustup target add riscv32i-unknown-none-elf
 
 # Install Risc Zero
 curl -L https://risczero.com/install | bash
+source /home/runner/.bashrc
 rzup install
 
 # Install SP1
 curl -L https://sp1.succinct.xyz | bash
+source /home/runner/.bashrc
 sp1up
 
 # Install stone-cli
@@ -37,7 +40,7 @@ cargo install --git https://github.com/zksecurity/stone-cli.git --branch dynamic
 pip install cairo-lang
 
 # Install SHARP certificate for dynamic layout
-cd ~
-git clone git@github.com:zksecurity/sharp-cert.git
-cd sharp-cert/
+REPO_URL="https://x-access-token:${PERSONAL_ACCESS_TOKEN}@github.com/zksecurity/sharp-cert.git"
+git clone "${REPO_URL}" ~/sharp-cert
+cd ~/sharp-cert
 age --decrypt -o user.key user.key.age

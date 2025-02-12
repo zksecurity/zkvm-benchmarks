1e79ad1a04552011850adb10a8edbccb4c1c4b37
varunthakore <vrnthakore@gmail.com>
2025-02-12 06:27:31 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,40 @@ echo "Setting up benchmark..."
 
 whoami
 
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install other dependencies
 sudo apt update -y
 sudo apt install -y heaptrack
+sudo apt install -y build-essential
+sudo apt install -y pkg-config
+sudo apt install -y libssl-dev
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo apt install -y libgmp-dev
+sudo apt install -y clang
+sudo apt install -y age
+
+# Install Jolt
+rustup target add riscv32i-unknown-none-elf
+
+# Install Risc Zero
+curl -L https://risczero.com/install | bash
+rzup install
+
+# Install SP1
+curl -L https://sp1.succinct.xyz | bash
+sp1up
+
+# Install stone-cli
+cargo install --git https://github.com/zksecurity/stone-cli.git --branch dynamic-layout
+
+# Install cairo
+pip install cairo-lang
+
+# Install SHARP certificate for dynamic layout
+cd ~
+git clone git@github.com:zksecurity/sharp-cert.git
+cd sharp-cert/
+age --decrypt -o user.key user.key.age

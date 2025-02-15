14845ac407aa96dd6539ace464e2d7b7889364f6
varunthakore <vrnthakore@gmail.com>
2025-02-15 09:19:02 +0000

    Update Setup

--- setup.sh
+++ setup.sh
@@ -9,18 +9,18 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 export PATH="$HOME/.cargo/env:$PATH"
 
+# Install Python3.10
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install -y python3.10 python3.10-venv python3.10-distutils
+
 # Install other dependencies
 sudo apt update -y
 sudo apt install -y heaptrack
 sudo apt install -y build-essential
 sudo apt install -y pkg-config
 sudo apt install -y libssl-dev
-
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update -y
-sudo apt install -y python3.10 python3.10-venv python3.10-distutils
-
 sudo apt install -y libgmp-dev
 sudo apt install -y clang
 sudo apt install -y age
@@ -53,7 +53,6 @@ pip install cairo-lang
 
 # Install stone-cli
 cargo install --git https://github.com/zksecurity/stone-cli.git --branch dynamic-layout
-stone-cli --version || echo "stone-cli installation failed"
 
 # Check Setup
 rustc --version || echo "Rust installation failed"

fe92937d98ea741acef9cb822e98989ed96a61bf
varunthakore <vrnthakore@gmail.com>
2025-02-16 11:58:57 +0000

    Update SP1 to V4

---
+++ sp1/build.rs
@@ -0,0 +1,17 @@
+use sp1_build::build_program_with_args;
+
+fn main() {
+    build_program_with_args("fibonacci", Default::default());
+    build_program_with_args("sha2", Default::default());
+    build_program_with_args("sha2-precompile", Default::default());
+    build_program_with_args("sha2-chain", Default::default());
+    build_program_with_args("sha2-chain-precompile", Default::default());
+    build_program_with_args("sha3-chain", Default::default());
+    build_program_with_args("sha3-chain-precompile", Default::default());
+    build_program_with_args("sha3", Default::default());
+    build_program_with_args("sha3-precompile", Default::default());
+    build_program_with_args("mat-mul", Default::default());
+    build_program_with_args("binary-search", Default::default());
+    build_program_with_args("ec", Default::default());
+    build_program_with_args("ec-precompile", Default::default());
+}
--- sp1/src/main.rs
+++ sp1/src/main.rs
@@ -1,21 +1,24 @@
-use std::{io::Write, time::{Duration, Instant}};
+use std::{
+    io::Write,
+    time::{Duration, Instant},
+};
 
-use sp1_sdk::{utils as sp1_utils, ProverClient, SP1Stdin};
+use sp1_sdk::{include_elf, utils as sp1_utils, ProverClient, SP1Stdin};
 use utils::size;
 
-const FIBONACCI_ELF: &[u8] = include_bytes!("../fibonacci/elf/riscv32im-succinct-zkvm-elf");
-const SHA2_ELF: &[u8] = include_bytes!("../sha2/elf/riscv32im-succinct-zkvm-elf");
-const SHA2_PRECOMPILE_ELF: &[u8] = include_bytes!("../sha2-precompile/elf/riscv32im-succinct-zkvm-elf");
-const SHA2_CHAIN_ELF: &[u8] = include_bytes!("../sha2-chain/elf/riscv32im-succinct-zkvm-elf");
-const SHA2_CHAIN_PRECOMPILE_ELF: &[u8] = include_bytes!("../sha2-chain-precompile/elf/riscv32im-succinct-zkvm-elf");
-const SHA3_CHAIN_ELF: &[u8] = include_bytes!("../sha2-chain/elf/riscv32im-succinct-zkvm-elf");
-const SHA3_CHAIN_PRECOMPILE_ELF: &[u8] = include_bytes!("../sha3-chain-precompile/elf/riscv32im-succinct-zkvm-elf");
-const SHA3_ELF: &[u8] = include_bytes!("../sha3/elf/riscv32im-succinct-zkvm-elf");
-const SHA3_PRECOMPILE_ELF: &[u8] = include_bytes!("../sha3-precompile/elf/riscv32im-succinct-zkvm-elf");
-const MATMUL_ELF: &[u8] = include_bytes!("../mat-mul/elf/riscv32im-succinct-zkvm-elf");
-const BINARY_SEARCH_ELF: &[u8] = include_bytes!("../binary-search/elf/riscv32im-succinct-zkvm-elf");
-const ECADD_ELF: &[u8] = include_bytes!("../ec/elf/riscv32im-succinct-zkvm-elf");
-const ECADD_PRECOMPILE_ELF: &[u8] = include_bytes!("../ec-precompile/elf/riscv32im-succinct-zkvm-elf");
+const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci");
+const SHA2_ELF: &[u8] = include_elf!("sha2");
+const SHA2_PRECOMPILE_ELF: &[u8] = include_elf!("sha2-precompile");
+const SHA2_CHAIN_ELF: &[u8] = include_elf!("sha2-chain");
+const SHA2_CHAIN_PRECOMPILE_ELF: &[u8] = include_elf!("sha2-chain-precompile");
+const SHA3_CHAIN_ELF: &[u8] = include_elf!("sha3-chain");
+const SHA3_CHAIN_PRECOMPILE_ELF: &[u8] = include_elf!("sha3-chain-precompile");
+const SHA3_ELF: &[u8] = include_elf!("sha3");
+const SHA3_PRECOMPILE_ELF: &[u8] = include_elf!("sha3-precompile");
+const MATMUL_ELF: &[u8] = include_elf!("mat-mul");
+const BINARY_SEARCH_ELF: &[u8] = include_elf!("binary-search");
+const ECADD_ELF: &[u8] = include_elf!("ec");
+const ECADD_PRECOMPILE_ELF: &[u8] = include_elf!("ec-precompile");
 
 use clap::Parser;
 
@@ -25,12 +28,11 @@ use clap::Parser;
 pub struct Cli {
     #[arg(long)]
     pub n: u32,
-    
+
     /// Run the benchmark under heaptrack for memory profiling
     #[arg(long)]
     pub program: String,
 }
-
 
 fn main() {
     sp1_utils::setup_logger();
@@ -38,54 +40,32 @@ fn main() {
     let cli = Cli::parse();
 
     let (duration, proof_size, verifier_duration, cycle_count) = match cli.program.as_str() {
-        "fib" => {
-            bench_fibonacci(cli.n)
-        },
-        "sha2" => {
-            benchmark_sha2(cli.n as usize)
-        },
-        "sha2-precompile" => {
-            benchmark_sha2_precompile(cli.n as usize)
-        },
-        "sha2-chain" => {
-            benchmark_sha2_chain(cli.n)
-        },
-        "sha2-chain-precompile" => {
-            benchmark_sha2_chain_precompile(cli.n)
-        },
-        "sha3" => {
-            benchmark_sha3(cli.n as usize)
-        },
-        "sha3-precompile" => {
-            benchmark_sha3_precompile(cli.n as usize)
-        },
-        "sha3-chain" => {
-            benchmark_sha3_chain(cli.n)
-        },
-        "sha3-chain-precompile" => {
-            benchmark_sha3_chain_precompile(cli.n)
-        },
-        "mat-mul" => {
-            bench_mat_mul(cli.n)
-        },
-        "binary-search" => {
-            benchmark_binary_search(cli.n as usize)
-        },
-        "ecadd" => {
-            bench_ecadd(cli.n)
-        },
-        "ecadd-precompile" => {
-            bench_ecadd_precompile(cli.n)
-        }
-        _ => unreachable!()
-
+        "fib" => bench_fibonacci(cli.n),
+        "sha2" => benchmark_sha2(cli.n as usize),
+        "sha2-precompile" => benchmark_sha2_precompile(cli.n as usize),
+        "sha2-chain" => benchmark_sha2_chain(cli.n),
+        "sha2-chain-precompile" => benchmark_sha2_chain_precompile(cli.n),
+        "sha3" => benchmark_sha3(cli.n as usize),
+        "sha3-precompile" => benchmark_sha3_precompile(cli.n as usize),
+        "sha3-chain" => benchmark_sha3_chain(cli.n),
+        "sha3-chain-precompile" => benchmark_sha3_chain_precompile(cli.n),
+        "mat-mul" => bench_mat_mul(cli.n),
+        "binary-search" => benchmark_binary_search(cli.n as usize),
+        "ecadd" => bench_ecadd(cli.n),
+        "ecadd-precompile" => bench_ecadd_precompile(cli.n),
+        _ => unreachable!(),
     };
     let mut file = std::fs::File::create("results.json").unwrap();
     file.write_all(format!("{{\"proof_size\": {}, \"duration\": {}, \"verifier_duration\": {}, \"cycle_count\": {}}}", proof_size, duration.as_millis(), verifier_duration.as_millis(), cycle_count).as_bytes()).unwrap();
-
 }
 
-fn _benchmark_with_shard_size(func: fn(u32) -> (Duration, usize), iters: &[u32], shard_sizes: &[usize], file_name: &str, input_name: &str) {
+fn _benchmark_with_shard_size(
+    func: fn(u32) -> (Duration, usize),
+    iters: &[u32],
+    shard_sizes: &[usize],
+    file_name: &str,
+    input_name: &str,
+) {
     assert_eq!(iters.len(), shard_sizes.len());
     let mut info = Vec::new();
     for bench_i in 0..iters.len() {
@@ -102,13 +82,13 @@ fn benchmark_sha2_chain(iters: u32) -> (Duration, usize, Duration, usize) {
     stdin.write(&input);
     stdin.write(&iters);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA2_CHAIN_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(SHA2_CHAIN_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA2_CHAIN_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -126,13 +106,16 @@ fn benchmark_sha2_chain_precompile(iters: u32) -> (Duration, usize, Duration, us
     stdin.write(&input);
     stdin.write(&iters);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA2_CHAIN_PRECOMPILE_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client
+        .execute(SHA2_CHAIN_PRECOMPILE_ELF, &stdin)
+        .run()
+        .unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA2_CHAIN_PRECOMPILE_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -150,13 +133,13 @@ fn benchmark_sha3_chain(iters: u32) -> (Duration, usize, Duration, usize) {
     stdin.write(&input);
     stdin.write(&iters);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA3_CHAIN_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(SHA3_CHAIN_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA3_CHAIN_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -174,13 +157,16 @@ fn benchmark_sha3_chain_precompile(iters: u32) -> (Duration, usize, Duration, us
     stdin.write(&input);
     stdin.write(&iters);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA3_CHAIN_PRECOMPILE_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client
+        .execute(SHA3_CHAIN_PRECOMPILE_ELF, &stdin)
+        .run()
+        .unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA3_CHAIN_PRECOMPILE_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -197,13 +183,13 @@ fn benchmark_sha2(num_bytes: usize) -> (Duration, usize, Duration, usize) {
     let input = vec![5u8; num_bytes];
     stdin.write(&input);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA2_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(SHA2_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA2_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -220,13 +206,13 @@ fn benchmark_sha2_precompile(num_bytes: usize) -> (Duration, usize, Duration, us
     let input = vec![5u8; num_bytes];
     stdin.write(&input);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA2_PRECOMPILE_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(SHA2_PRECOMPILE_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA2_PRECOMPILE_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -243,13 +229,13 @@ fn benchmark_sha3_precompile(num_bytes: usize) -> (Duration, usize, Duration, us
     let input = vec![5u8; num_bytes];
     stdin.write(&input);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA3_PRECOMPILE_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(SHA3_PRECOMPILE_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA3_PRECOMPILE_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -266,13 +252,13 @@ fn benchmark_sha3(num_bytes: usize) -> (Duration, usize, Duration, usize) {
     let input = vec![5u8; num_bytes];
     stdin.write(&input);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(SHA3_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(SHA3_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(SHA3_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -288,13 +274,13 @@ fn bench_fibonacci(n: u32) -> (Duration, usize, Duration, usize) {
     let mut stdin = SP1Stdin::new();
     stdin.write(&n);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(FIBONACCI_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(FIBONACCI_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(FIBONACCI_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -310,13 +296,13 @@ fn bench_ecadd(n: u32) -> (Duration, usize, Duration, usize) {
     let mut stdin = SP1Stdin::new();
     stdin.write(&n);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(ECADD_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(ECADD_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(ECADD_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -332,13 +318,13 @@ fn bench_ecadd_precompile(n: u32) -> (Duration, usize, Duration, usize) {
     let mut stdin = SP1Stdin::new();
     stdin.write(&n);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(ECADD_PRECOMPILE_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(ECADD_PRECOMPILE_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(ECADD_PRECOMPILE_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -352,17 +338,17 @@ fn bench_ecadd_precompile(n: u32) -> (Duration, usize, Duration, usize) {
 
 fn benchmark_binary_search(n: usize) -> (Duration, usize, Duration, usize) {
     let input: Vec<usize> = (1..=n).collect();
-    
+
     let mut stdin = SP1Stdin::new();
     stdin.write(&input);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(BINARY_SEARCH_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(BINARY_SEARCH_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(BINARY_SEARCH_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 
@@ -378,13 +364,13 @@ fn bench_mat_mul(n: u32) -> (Duration, usize, Duration, usize) {
     let mut stdin = SP1Stdin::new();
     stdin.write(&n);
 
-    let client = ProverClient::new();
-    let (_, report) = client.execute(MATMUL_ELF, stdin.clone()).run().unwrap();
-    let cycle_count =  report.total_instruction_count() as usize;
+    let client = ProverClient::from_env();
+    let (_, report) = client.execute(MATMUL_ELF, &stdin).run().unwrap();
+    let cycle_count = report.total_instruction_count() as usize;
     let (pk, vk) = client.setup(MATMUL_ELF);
 
     let start = Instant::now();
-    let proof = client.prove(&pk, stdin).run().unwrap();
+    let proof = client.prove(&pk, &stdin).run().unwrap();
     let end = Instant::now();
     let duration = end.duration_since(start);
 

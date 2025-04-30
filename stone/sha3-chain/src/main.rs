b4dfc78836faeb978c6ce1100843b983e2207509
varunthakore <vrnthakore@gmail.com>
2025-04-30 09:03:42 +0000

    Update sha-chain config

--- stone/sha3-chain/src/main.rs
+++ stone/sha3-chain/src/main.rs
@@ -48,17 +48,16 @@ fn run(n: u32) {
     let output_file = format!("proof_{}.json", n);
     let layout = "automatic".to_string();
     let parameter_file = match n {
-        15 => "../configs/parameter_2097152.json".to_string(),
-        29 => "../configs/parameter_2097152.json".to_string(),
-        58 => "../configs/parameter_2097152.json".to_string(),
-        115 => "../configs/parameter_4194304.json".to_string(),
-        230 => "../configs/parameter_8388608.json".to_string(),
-        460 => "../configs/parameter_16777216.json".to_string(),
-        920 => "../configs/parameter_33554432.json".to_string(),
-        1840 => "../configs/parameter_67108864.json".to_string(),
-        3680 => "../configs/parameter_134217728.json".to_string(),
+        8 => "../configs/parameter_524288.json".to_string(),
+        16 => "../configs/parameter_1048576.json".to_string(),
+        32 => "../configs/parameter_2097152.json".to_string(),
+        64 => "../configs/parameter_4194304.json".to_string(),
+        128 => "../configs/parameter_8388608.json".to_string(),
+        256 => "../configs/parameter_16777216.json".to_string(),
+        512 => "../configs/parameter_33554432.json".to_string(),
+        1024 => "../configs/parameter_67108864.json".to_string(),
         _ => unreachable!("Unexpected value for n: {}", n),
-    };    
+    };
     let prover_config_file = "../configs/prover_config.json".to_string();
 
     let args = vec![

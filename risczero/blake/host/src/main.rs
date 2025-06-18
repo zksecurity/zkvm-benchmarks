307bd443a18d37ad08cd74dd9cf921ae69bd0976
varunthakore <vrnthakore@gmail.com>
2025-06-18 18:53:51 +0000

    add r0 blake

---
+++ risczero/blake/host/src/main.rs
@@ -0,0 +1,43 @@
+use std::time::Duration;
+
+use methods::{BLAKE_BENCH_ELF, BLAKE_BENCH_ID};
+use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts};
+use utils::size;
+use std::io::Write;
+
+fn main() {
+    // read the first arg
+    let args: Vec<String> = std::env::args().collect();
+    let n = args.iter().position(|arg| arg == "--n")
+        .and_then(|i| args.get(i + 1))
+        .expect("Please provide --n <number>")
+        .parse::<usize>()
+        .expect("Invalid number");
+
+    let (duration, proof_size, verifier_duration, cycle_count) = bench_blake(n);
+    let mut file = std::fs::File::create("results.json").unwrap();
+    file.write_all(format!("{{\"proof_size\": {}, \"duration\": {}, \"verifier_duration\": {}, \"cycle_count\": {}}}", proof_size, duration.as_millis(), verifier_duration.as_millis(), cycle_count).as_bytes()).unwrap();
+}
+
+fn bench_blake(num_bytes: usize) -> (Duration, usize, Duration, usize) {
+    let input = vec![5u8; num_bytes];
+    let env = ExecutorEnv::builder().write(&input).unwrap().build().unwrap();
+    let prover = default_prover();
+
+    let start = std::time::Instant::now();
+    let prove_info = prover.prove_with_opts(env, BLAKE_BENCH_ELF, &ProverOpts::succinct()).unwrap();
+    let end = std::time::Instant::now();
+    let duration = end.duration_since(start);
+
+    let receipt = prove_info.receipt;
+    let cycle_count = prove_info.stats.user_cycles as usize;
+
+    let _output: [u8; 32] = receipt.journal.decode().unwrap();
+
+    let verifier_start = std::time::Instant::now();
+    receipt.verify(BLAKE_BENCH_ID).unwrap();
+    let verifier_end = std::time::Instant::now();
+    let verifier_duration = verifier_end.duration_since(verifier_start);
+
+    (duration, size(&receipt), verifier_duration, cycle_count)
+}
